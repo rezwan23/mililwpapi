5acf4dfc1b9b683cdcd76fec9130f2fda2b75ef9
rezwan23 <razwanulghani@gmail.com>
2021-07-12 15:24:08 +0600

    test

--- Milil.php
+++ Milil.php
@@ -25,6 +25,9 @@ class Milil{
     public function includeShortCodes()
     {
         $shortCodeFiles = scandir(getcwd().DIRECTORY_SEPARATOR.'shortcodes');
+
+        die($shortCodeFiles);
+
         foreach($shortCodeFiles as $file){
             if(!is_dir($file)){
                 require_once $file;
